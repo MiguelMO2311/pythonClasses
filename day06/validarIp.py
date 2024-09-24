9cb03d1f38f102d343e2174668fe80499a6e433c
MiguelMO2311 <mmeneses73@gmail.com>
2024-09-24 11:50:03 +0200

    day06 modif

--- day06/validarIp.py
+++ day06/validarIp.py
@@ -19,3 +19,24 @@ if validar_ip(ip):
     print(f"La IP {ip} es válida.")
 else:
     print(f"La IP {ip} no es válida.")
+
+''' Misma funcion pero con el metodo IPADDRESS
+
+import ipaddress
+
+def validar_ip(ip: str) -> bool:
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+
+# Solicitar la IP al usuario
+ip = input("Introduce una dirección IP: ")
+if validar_ip(ip):
+    print(f"La IP {ip} es válida.")
+else:
+    print(f"La IP {ip} no es válida.")
+
+    
+'''
